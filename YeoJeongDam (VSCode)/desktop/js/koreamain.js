bb26d3b513babc8926f708866bc0377bb8ed53ec
yeeunrim <1515eun@daum.net>
2023-12-12 11:28:01 +0900

    upload20231212

---
+++ YeoJeongDam (VSCode)/desktop/js/koreamain.js
@@ -0,0 +1,56 @@
+// 지역 선택 - 드롭다운
+function dropdown() {
+  var v = document.querySelector('.dropdown-content');
+  var dropbtn = document.querySelector('.dropbtn')
+  v.classList.toggle('show');
+  dropbtn.style.borderColor = 'rgb(94, 94, 94)';
+}
+
+function dropdown2() {
+  var v = document.querySelector('#dropdown-content2');
+  var dropbtn = document.querySelector('#dropbtn2')
+  v.classList.toggle('show');
+  dropbtn.style.borderColor = 'rgb(94, 94, 94)';
+}
+
+function dropdown3() {
+  var v = document.querySelector('#dropdown-content3');
+  var dropbtn = document.querySelector('#dropbtn3')
+  v.classList.toggle('show');
+  dropbtn.style.borderColor = 'rgb(94, 94, 94)';
+}
+
+function dropdown4() {
+  var v = document.querySelector('#dropdown-content4');
+  var dropbtn = document.querySelector('#dropbtn4')
+  v.classList.toggle('show');
+  dropbtn.style.borderColor = 'rgb(94, 94, 94)';
+}
+
+function dropdown5() {
+  var v = document.querySelector('#dropdown-content5');
+  var dropbtn = document.querySelector('#dropbtn5')
+  v.classList.toggle('show');
+  dropbtn.style.borderColor = 'rgb(94, 94, 94)';
+}
+
+function dropdown6() {
+  var v = document.querySelector('#dropdown-content6');
+  var dropbtn = document.querySelector('#dropbtn6')
+  v.classList.toggle('show');
+  dropbtn.style.borderColor = 'rgb(94, 94, 94)';
+}
+
+function dropdown7() {
+  var v = document.querySelector('#dropdown-content7');
+  var dropbtn = document.querySelector('#dropbtn7')
+  v.classList.toggle('show');
+  dropbtn.style.borderColor = 'rgb(94, 94, 94)';
+}
+
+function dropdown8() {
+  var v = document.querySelector('#dropdown-content8');
+  var dropbtn = document.querySelector('#dropbtn8')
+  v.classList.toggle('show');
+  dropbtn.style.borderColor = 'rgb(94, 94, 94)';
+}

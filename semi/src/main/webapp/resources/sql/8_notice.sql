f6fe2f31386d596ae1ad22ff2805411dce99425e
gaeunlee95 <yuebn22@naver.com>
2023-12-18 01:10:44 +0900

    진짜 final

--- semi/src/main/webapp/resources/sql/8_notice.sql
+++ semi/src/main/webapp/resources/sql/8_notice.sql
@@ -11,13 +11,13 @@ CREATE TABLE notice (
     nlike_count int,
     FOREIGN KEY(id) REFERENCES users(id) ON DELETE CASCADE    
 );
-DROP TABLE notice;
 
 CREATE SEQUENCE seq_nno NOCACHE;
 
-INSERT INTO notice(nno, ntitle, ncontent, id)
-VALUES (seq_nno.NEXTVAL, '공지사항', '공지사항입니다.', 'khit');
-
+INSERT INTO notice(nno, ntitle, ncontent, id) VALUES (seq_nno.NEXTVAL, '여행지 안전 수칙 안내', '최근 안전 문제로 인해 여행지의 안전 수칙이 강화되었습니다. 여행 전 반드시 확인해 주세요.', '관리자');
+INSERT INTO notice(nno, ntitle, ncontent, id) VALUES (seq_nno.NEXTVAL, '웹사이트 점검 안내', '시스템 개선을 위한 웹사이트 점검이 진행될 예정입니다. 이용에 불편을 드려 죄송합니다.', '관리자');
+INSERT INTO notice(nno, ntitle, ncontent, id) VALUES (seq_nno.NEXTVAL, '회원 가입 혜택 안내', '신규 회원 가입 시 다양한 혜택을 제공합니다. 지금 바로 가입하시고 혜택을 누리세요!', '관리자');
+INSERT INTO notice(nno, ntitle, ncontent, id) VALUES (seq_nno.NEXTVAL, '여행 후기 공모전 개최', '여행 후기 공모전을 개최합니다. 참가하시어 소중한 경험을 공유해 주세요. 다양한 상품이 준비되어 있습니다.', 'show3239');
 commit;
 
 select * from notice;
@@ -39,15 +39,16 @@ DROP TABLE nreply;
 CREATE SEQUENCE seq_nrno NOCACHE;
 
 INSERT INTO nreply(nrno, nno, nrcontent, nreplyer)
-VALUES (seq_nrno.NEXTVAL, 4, '첫번째 댓글입니다.', 'khit');
+VALUES (seq_nrno.NEXTVAL, 1, '첫번째 댓글입니다.', 'show3239');
 
 CREATE TABLE nlike(
     nlikeno NUMBER PRIMARY KEY,
     nno    NUMBER NOT NULL,
     id     VARCHAR2(20) NOT NULL,
     FOREIGN KEY (nno) REFERENCES notice(nno) ON DELETE CASCADE,
-    FOREIGN KEY(id) REFERENCES member(id) ON DELETE CASCADE   
+    FOREIGN KEY(id) REFERENCES users(id) ON DELETE CASCADE   
 );
 DROP TABLE nlike;
 
 CREATE SEQUENCE seq_nlikeno NOCACHE;
+
